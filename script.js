b03d3fb4311c3120fba0d02c5544405b313b77da
Tabita Costa <114258195+TabitaCosta@users.noreply.github.com>
2025-01-31 19:35:22 -0300

    Update script.js
    adiciona botão para baixar a imagem

--- script.js
+++ script.js
@@ -1,17 +1,35 @@
-
 function generateQRCode() {
     var inputValue = document.getElementById("qr-input").value;
     var qrcodeContainer = document.getElementById("qrcode");
+    var downloadButton = document.getElementById("download-btn");
 
     if (inputValue.trim() !== "") {
-        qrcodeContainer.innerHTML = ""; // Limpar o conteúdo anterior, se houver
-
+        qrcodeContainer.innerHTML = ""; 
         var qrcode = new QRCode(qrcodeContainer, {
             text: inputValue,
             width: 200,
             height: 200
         });
+
+        setTimeout(() => {
+            downloadButton.style.display = "inline-block";
+        }, 300); 
+
     } else {
         alert("Por favor, insira um texto para gerar o QR Code.");
+    }
+}
+
+function downloadQRCode() {
+    var qrCodeImg = document.querySelector("#qrcode img");
+
+    if (qrCodeImg) {
+        var imgURL = qrCodeImg.src;
+        var link = document.createElement("a");
+        link.href = imgURL;
+        link.download = "qrcode.png";
+        link.click();
+    } else {
+        alert("Primeiro, gere o QR Code antes de baixar.");
     }
 }
